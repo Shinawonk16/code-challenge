349ba30b790d2867b69e89aad887729ff935f46e
Shinawonk16 <shinawonk@gmail.com>
2022-10-14 04:10:44 -0700

    first comit

---
+++ code two/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace code_two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name ="sheriffdeen abdul wareez";
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
